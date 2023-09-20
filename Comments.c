bce8112ce0ad81700c4e36d89c731ec67c219267
Soheb Shafi Tamboli <120311878+soheb123-tamboli@users.noreply.github.com>
2023-09-20 21:54:49 +0530

    Create Comments.c

---
+++ Comments.c
@@ -0,0 +1,16 @@
+// Their are two types of comments.
+// 1) Single line comments 
+/*2) Multi-line comments*/
+
+/*
+  Author:Soheb;
+  Topic :Comments
+*/
+#include<stdio.h>
+int main() {
+  char name[15]="Form House";     //Pizza name is farm house
+  char size='M';     //person it serves is 2
+  float priceIncludingTax=365.8;  //price including
+  print("%s",name);
+  return 0;
+}
